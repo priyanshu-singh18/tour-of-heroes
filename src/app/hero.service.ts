70436b4236c3375db72c1c56840b9b5acf9a4f45
“Priyanshu <priyanshu@spotdraft.com>
2023-08-29 02:36:11 +0530

    add message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes() : Hero[]{
-    return HEROES;
-  }
+  constructor(private messageService : MessageService){}
 
-  constructor() { }
+  getHeroes() : Observable<Hero[]>{
+    const heroes = of(HEROES);
+    this.messageService.add("Heroes Fetched !!");
+    return heroes;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+selectedHero?: Hero;
+  
  heroes : Hero[] = [];
 
- constructor(private heroService : HeroService){}
+ constructor(private heroService : HeroService, private messageService : MessageService){}
 
  getHeroes() : void {
-  this.heroes = this.heroService.getHeroes();
+   this.heroService.getHeroes().subscribe(heroes=>this.heroes = heroes)
  }
 
  ngOnInit():void{
   this.getHeroes();
  }
- selectedHero?: Hero;
-onSelect(hero: Hero): void {
+  onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  this.messageService.add(`Hero Service : Selected Hero id:${hero.id}`)
 }
 }
