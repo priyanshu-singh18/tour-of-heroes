9494af87c340583a14e195312ebc0cd863b5fab6
“Priyanshu <priyanshu@spotdraft.com>
2023-08-29 00:59:35 +0530

    add hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,8 +8,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
- heroes = HEROES
+ heroes : Hero[] = [];
 
+ constructor(private heroService : HeroService){}
+
+ getHeroes() : void {
+  this.heroes = this.heroService.getHeroes();
+ }
+
+ ngOnInit():void{
+  this.getHeroes();
+ }
  selectedHero?: Hero;
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
