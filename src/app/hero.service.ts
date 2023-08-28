937826d9b8200d3c91835a8b325b2a790752509a
“Priyanshu <priyanshu@spotdraft.com>
2023-08-29 04:17:36 +0530

    add navigation and dashboard

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,7 +15,13 @@ export class HeroService {
 
   getHeroes() : Observable<Hero[]>{
     const heroes = of(HEROES);
-    this.messageService.add("Heroes Fetched !!");
+    this.messageService.add("Hero Service : Heroes Fetched !!");
     return heroes;
+  }
+
+  getHero(id : number) : Observable<Hero | undefined>{
+    const hero = HEROES.find(hero => hero.id === id);
+    this.messageService.add(`Hero Service : Hero Fetched with id=${id}` )
+    return of(hero);
   }
 }
